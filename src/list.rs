e3825305d28e7a4ec4a1efeec6be5aaa8d1f26d4
xukaijie2022@126.com <xukaijie2022@126.com>
2024-02-22 17:35:45 +0800

    保存代码

--- src/list.rs
+++ src/list.rs
@@ -1,62 +1,54 @@
-
 use std::{fs::read_link, path::PathBuf};
 
 use anyhow::Result;
 use clap::Parser;
 
-use crate::{package::{self, find_qr_package_files, get_npm_path, get_packages_by_files, DepReq, ListTarget, Package}, Command};
+use crate::{
+    package::{
+        find_qr_package_files, get_npm_path, get_packages_by_files, ListTarget
+    },
+    Command,
+};
 
 /**
- * 
+ *
  * 列出链接的信息
  */
 
 #[derive(Parser, Clone, Debug)]
 #[command(about = "list qr_mini_pay link list ", alias = "s")]
 pub struct ListCommand {
-    /// the source diretory. emenu-mini-core
+    /// the source diretory. qr_mini_pay
     #[arg(long, short)]
     pub path: PathBuf,
 }
 
-
-
 impl ListCommand {
-
     pub fn run(command: &Command, options: &ListCommand) -> Result<()> {
-
         let path = options.path.to_owned();
         let target_package_files = find_qr_package_files(&path).unwrap();
 
         let packages = get_packages_by_files(&target_package_files);
 
+        let node_modules_paths = get_npm_path(&packages, "node_modules").unwrap();
 
-        let  node_modules_paths = get_npm_path(&packages,"node_modules").unwrap();
+        let miniprogram_paths = get_npm_path(&packages, "miniprogram_npm").unwrap();
 
-        let miniprogram_paths = get_npm_path(&packages,"miniprogram_npm").unwrap();
-
-
-        let lists = [node_modules_paths,miniprogram_paths].concat();
-
+        let lists = [node_modules_paths, miniprogram_paths].concat();
 
         Self::print(&lists).unwrap();
 
-
         Ok(())
-
     }
 
-
-    pub fn print(lists:&Vec<ListTarget>)->Result<()> {
-
+    pub fn print(lists: &Vec<ListTarget>) -> Result<()> {
         let mut has_link = false;
         for ele in lists {
             let path = ele.path.to_owned();
             let res = read_link(&path);
-            if res.is_ok(){
-                print!("{:?}  <----  {:?}\n",path,res.unwrap());
+            if res.is_ok() {
+                print!("{:?}  <----  {:?}\n", path, res.unwrap());
                 has_link = true;
-
             }
         }
 
@@ -66,9 +58,4 @@ impl ListCommand {
 
         Ok(())
     }
-
-
-
-
-
 }
